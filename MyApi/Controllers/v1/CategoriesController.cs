45b9eecc94aa46fe105d54db59094a03ae48fa8b
driars <driars0329@gmail.com>
2023-08-24 22:50:55 +0200

    Remove warnings on auth system

--- MyApi/Controllers/v1/CategoriesController.cs
+++ MyApi/Controllers/v1/CategoriesController.cs
@@ -7,7 +7,6 @@ using WebFramework.Api;
 
 namespace MyApi.Controllers.v1
 {
-    [AllowAnonymous]
     public class CategoriesController : CrudController<CategoryDto, CategorySelectDto, Category>
     {
         public CategoriesController(IRepository<Category> repository, IMapper mapper)
